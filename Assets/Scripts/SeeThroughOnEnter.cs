8541fc5cdb9f3fa145e93d23f0226a364f7820fd
Ieva Dapševičiūtė <ievdps@telesoftas.com>
2019-09-09 11:03:09 +0300

    Implement SeeThroughOnEnter script

---
+++ Assets/Scripts/SeeThroughOnEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThroughOnEnter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Player") return;
+
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 0f;
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name != "Player") return;
+
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 1f;
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+}
