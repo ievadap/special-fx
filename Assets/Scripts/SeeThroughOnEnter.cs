a5d1f18aaa3c7d9ca3438637124789433fd1e49a
Ieva Dapševičiūtė <ievdps@telesoftas.com>
2019-09-30 10:46:34 +0300

    Modify SeeThroughOnEnter to fluctuate transparency while not triggered

--- Assets/Scripts/SeeThroughOnEnter.cs
+++ Assets/Scripts/SeeThroughOnEnter.cs
@@ -1,24 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SeeThroughOnEnter : MonoBehaviour
 {
+    private bool isTriggered = false;
+    private new Renderer renderer;
+    private void Start() 
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    private void Update() 
+    {
+        animateTransparency();
+    }
+    private void animateTransparency()
+    {
+        if (isTriggered) return;
+        float alpha = Mathf.PingPong(Time.time, 1.0f);
+        renderer.material.SetColor("_Color", colorWithAlpha(alpha)); 
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name != "Player") return;
-
-        Color color = GetComponent<Renderer>().material.color;
-        color.a = 0f;
-        GetComponent<Renderer>().material.SetColor("_Color", color);
+        isTriggered = true;
+        renderer.material.SetColor("_Color", colorWithAlpha(0));
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.name != "Player") return;
-
-        Color color = GetComponent<Renderer>().material.color;
-        color.a = 1f;
-        GetComponent<Renderer>().material.SetColor("_Color", color);
+        isTriggered = false;
+        renderer.material.SetColor("_Color", colorWithAlpha(1));
+    }
+    private Color colorWithAlpha(float alpha) 
+    {
+        Color color = renderer.material.color;
+        color.a = alpha;
+        return color;
     }
 }
