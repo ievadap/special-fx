b1048e73b59bc1aa9b8c36719af81ea09ec865bd
Ieva Dapševičiūtė <ievdps@telesoftas.com>
2019-09-30 10:46:54 +0300

    Create Deglossify.cs

---
+++ Assets/Scripts/Deglossify.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deglossify : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Player") return;
+        GetComponent<Renderer>().material.SetFloat("_Glossiness", 0f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name != "Player") return;
+        GetComponent<Renderer>().material.SetFloat("_Glossiness", 1f);
+    }
+}
