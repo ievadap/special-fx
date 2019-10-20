723b3e60cdb6bb13cb42914f343cb4f310c3cfa1
Ieva Dapševičiūtė <ieva.dapseviciute@telesoftas.com>
2019-10-20 12:50:49 +0300

    Implement PPPTimer.cs

---
+++ Assets/PPPTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPPTimer : MonoBehaviour
+{
+    Bloom bloomLayer;
+    private void Update() {
+        PostProcessVolume volume = gameObject.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out bloomLayer);
+        bloomLayer.intensity.value = Mathf.PingPong(Time.time, 1) * 25;
+    }
+}
